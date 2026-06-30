f59fb17470b745f12d3dc7684b6cd1be39077a74
setayesh <s.darvishi8521@gmail.com>
2026-06-30 19:03:26 +0330

    create nav bar items

---
+++ src/components/NavItems.jsx
@@ -0,0 +1,48 @@
+import AllTasksSvg from "./svg/AllTasksSvg";
+import CategorySvg from "./svg/CategorySvg";
+import CreateTasksSvg from "./svg/CreateTasksSvg";
+import DashboardSvg from "./svg/DashboardSvg";
+
+function NavItems() {
+  return (
+    <div className="flex flex-col items-start gap-y-3 w-full mt-10">
+      <button
+        className="flex items-center gap-x-1 cursor-pointer
+      focus:bg-old-rose-50 hover:bg-old-rose-50 w-full py-2 transition-transform duration-500 pl-2
+      rounded-l-full ml-2 focus:shadow-[0px_10px_10px_-10px_#ce6489]"
+      >
+        <DashboardSvg />
+        <span className="span-style">Dashboard</span>
+      </button>
+
+      <button
+        className="flex items-center gap-x-1 cursor-pointer
+      focus:bg-old-rose-50 hover:bg-old-rose-50 w-full py-2 transition-transform duration-500 pl-2
+      rounded-l-full ml-2 focus:shadow-[0px_10px_10px_-10px_#ce6489]"
+      >
+        <AllTasksSvg />
+        <span className="span-style">All Tasks</span>
+      </button>
+
+      <button
+        className="flex items-center gap-x-1 cursor-pointer
+      focus:bg-old-rose-50 hover:bg-old-rose-50 w-full py-2 transition-transform duration-500 pl-2
+      rounded-l-full ml-2 focus:shadow-[0px_10px_10px_-10px_#ce6489]"
+      >
+        <CreateTasksSvg />
+        <span className="span-style">Create New Tasks</span>
+      </button>
+
+      <button
+        className="flex items-center gap-x-1 cursor-pointer
+      focus:bg-old-rose-50 hover:bg-old-rose-50 w-full py-2 transition-transform duration-500 pl-2
+      rounded-l-full ml-2 focus:shadow-[0px_10px_10px_-10px_#ce6489]"
+      >
+        <CategorySvg />
+        <span className="span-style">Category</span>
+      </button>
+    </div>
+  );
+}
+
+export default NavItems;
