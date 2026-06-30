53112b29140339e0f39b8bcb4cd75ced4c372910
setayesh <s.darvishi8521@gmail.com>
2026-06-30 18:32:34 +0330

    create svg

---
+++ src/components/svg/AllTasksSvg.jsx
@@ -0,0 +1,48 @@
+function AllTasksSvg() {
+  return (
+    <svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 1024 1024"
+      class="icon"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000000"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M553.490905 194.029341v219.8548h206.919033z"
+          fill="#f9ecf0"
+        ></path>
+        <path
+          d="M526.008799 441.366247V194.029341H278.671893v659.565424h494.673812V441.366247z"
+          fill="#f9ecf0"
+        ></path>
+        <path
+          d="M526.008799 166.547235v219.855824h206.919033z"
+          fill="#c23d6c"
+        ></path>
+        <path
+          d="M498.526693 413.884141V166.547235H251.189787v659.565424h494.673812V413.884141z"
+          fill="#c23d6c"
+        ></path>
+        <path
+          d="M348.737846 264.025003a24.724783 52.539652 55.515 1 0 86.614184-59.494899 24.724783 52.539652 55.515 1 0-86.614184 59.494899Z"
+          fill="#FEFEFE"
+        ></path>
+        <path
+          d="M295.645444 350.488564a16.48353 28.844897 55.515 1 0 47.552224-32.663412 16.48353 28.844897 55.515 1 0-47.552224 32.663412Z"
+          fill="#FEFEFE"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default AllTasksSvg;
